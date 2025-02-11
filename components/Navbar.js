06a26527dce2e0960d07bc32141f3f74147a1fbb
Abhinav Gupta <abhinavgupta2003.512@gmial.com>
2025-02-12 01:12:59 +0530

    Updation in Nav

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <nav className='flex justify-between md:h-16 h-8 items-center py-2 px-5 bg-[#052F5F] text-white'>
+    <nav className='flex justify-between md:h-16 h-12 items-center py-2 px-5 bg-[#052F5F] text-white'>
         <span className='font-bold text-2xl cursor-pointer'>ChopLink</span>
         <ul className='flex gap-5 text-lg'>
             <Link href="/" className='hover:bg-slate-50 hover:text-blue-900 px-1 py-1 cursor-pointer rounded-lg'>Home</Link>
